c5e89412cd4fd05fe1d4a42fb1b946682a2acd59
Aritro Ganguli <51890376+AritroGanguli@users.noreply.github.com>
2021-05-27 14:38:43 +0530

    Create Basic_Histogram_Plot.py
    Basic Histogram Plot of a sample normal distribution curve.

---
+++ Basic_Histogram_Plot.py
@@ -0,0 +1,15 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+def hist_plot_of_normal_distribution():
+  np.random.seed(100)
+  x1 = 25 + 3.0*np.random.randn(1000)
+
+  fig = plt.figure(figsize=(8,6))
+  ax = fig.add_subplot(111)
+  ax.set(title="Histogram of a Single Dataset",
+        ylabel='Bin Count', xlabel='X1')
+  ax.hist(x1, bins=30)
+  plt.savefig('histogram_norma.png')
